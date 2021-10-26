b8d909475f59d7b9e7e8500aef3a59506f95c16a
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-10-26 19:39:40 -0300

    Version 1.01

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StatusBar, Dimensions, Alert, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Dimensions, StyleSheet, ImageBackground, StatusBar } from 'react-native';
+import FoodImage from './src/assets/img/FoodImage.svg';
+import DrinkImage from './src/assets/img/DrinkImage.svg';
 
 export default function App() {
   const [protein, setProtein] = useState(0);
@@ -51,25 +53,25 @@ export default function App() {
     setResultDrink(0);
   };
 
-  switch (screen) {
-    case 'menu':
-      return getScreenMenu();
-    case 'food':
-      return getScreenFood();
-    case 'drink':
-      return getScreenDrink();
+  if (screen === 'menu') {
+    return getScreenMenu();
+  } else if (screen === 'food') {
+    return getScreenFood();
+  } else if (screen === 'drink') {
+    return getScreenDrink();
   };
 
   function getScreenMenu() {
     return (
-      <View>
-        <Text>Menu</Text>
-        <Text>About..........</Text>
+      <View style={Styles.containerMenu}>
+        <StatusBar backgroundColor="#170a28" />
+        <Text style={Styles.titleMenu}>Menu</Text>
+        <Text style={Styles.captionMenu}>Do you want to find out the amount of calories in a food or drink?</Text>
         <TouchableOpacity onPress={() => setScreen('food')} style={Styles.buttonFood}>
-          <Text>Food</Text>
+          <Text style={Styles.textButtonsMenu}>Food</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setScreen('drink')} style={Styles.buttonDrink}>
-          <Text>Drink</Text>
+          <Text style={Styles.textButtonsMenu}>Drink</Text>
         </TouchableOpacity>
       </View>
     );
@@ -77,35 +79,22 @@ export default function App() {
 
   function getScreenFood() {
     return (
-      <View>
-        <View>
-          <Text>Inform below how many grams of protein, carbohydrates and fat the food have</Text>
-        </View>
-        <View>
-          <Text>Protein</Text>
-          <TextInput keyboardType="numeric" onChangeText={text => proteins(text)} style={Styles.input} />
-        </View>
-        <View>
-          <Text>Carbohydrate</Text>
-          <TextInput keyboardType="numeric" onChangeText={text => carbohydrates(text)} style={Styles.input} />
-        </View>
-        <View>
-          <Text>Fat</Text>
-          <TextInput keyboardType="numeric" onChangeText={text => fats(text)} style={Styles.input} />
-        </View>
-        <View>
-          <TouchableOpacity onPress={calculateFood} style={Styles.button}>
-            <Text style={Styles.buttonText}>Calculate</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text>Your food have {resultFood} calories</Text>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => foodBacktoMenu()}>
-            <Text>Back to Menu</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={Styles.containerFood}>
+        <Text style={Styles.titleFood}>Inform below how many grams of protein, carbohydrates and fat the food have</Text>
+        <Text>Protein</Text>
+        <TextInput keyboardType="numeric" onChangeText={text => proteins(text)} style={Styles.input} />
+        <Text>Carbohydrate</Text>
+        <TextInput keyboardType="numeric" onChangeText={text => carbohydrates(text)} style={Styles.input} />
+        <Text>Fat</Text>
+        <TextInput keyboardType="numeric" onChangeText={text => fats(text)} style={Styles.input} />
+        <TouchableOpacity onPress={calculateFood} style={Styles.button}>
+          <Text style={Styles.buttonText}>Calculate</Text>
+        </TouchableOpacity>
+        <Text>Your food have {resultFood} calories</Text>
+        <TouchableOpacity onPress={() => foodBacktoMenu()}>
+          <Text>Back to Menu</Text>
+        </TouchableOpacity>
+        <FoodImage width="70%" />
       </View>
     );
   };
@@ -113,43 +102,72 @@ export default function App() {
   function getScreenDrink() {
     return (
       <View>
-        <View>
-          <Text>Inform bellow how many milliliters(ml) of drink you have and what's its alcohol by volume(ABV)</Text>
-        </View>
-        <View>
-          <Text>Milliliters(ml)</Text>
-          <TextInput keyboardType="numeric" onChangeText={text => milliliters(text)} style={Styles.input} />
-        </View>
-        <View>
-          <Text>Alcohol by Volume(ABV)</Text>
-          <TextInput keyboardType="numeric" onChangeText={text => alcohols(text)} style={Styles.input} />
-        </View>
-        <View>
-          <TouchableOpacity onPress={calculateDrink} style={Styles.button}>
-            <Text>Calculate</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text>Your drink have {resultDrink} calories</Text>
-        </View>
-        <View>
-          <TouchableOpacity onPress={() => drinkBackToMenu()}>
-            <Text>Back to Menu</Text>
-          </TouchableOpacity>
-        </View>
+        <Text>Inform bellow how many milliliters(ml) of drink you have and what's its alcohol by volume(ABV)</Text>
+        <Text>Milliliters(ml)</Text>
+        <TextInput keyboardType="numeric" onChangeText={text => milliliters(text)} style={Styles.input} />
+        <Text>Alcohol by Volume(ABV)</Text>
+        <TextInput keyboardType="numeric" onChangeText={text => alcohols(text)} style={Styles.input} />
+        <TouchableOpacity onPress={calculateDrink} style={Styles.button}>
+          <Text>Calculate</Text>
+        </TouchableOpacity>
+        <Text>Your drink have {resultDrink} calories</Text>
+        <TouchableOpacity onPress={() => drinkBackToMenu()}>
+          <Text>Back to Menu</Text>
+        </TouchableOpacity>
+        <DrinkImage width="80%" />
       </View>
     );
   };
 };
 
 const Styles = StyleSheet.create({
+  containerMenu: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#170a28',
+  },
+  titleMenu: {
+    fontSize: 45,
+    marginTop: -60,
+    marginBottom: 20,
+    color: '#fff',
+  },
+  captionMenu: {
+    fontSize: 20,
+    textAlign: 'center',
+    padding: 20,
+    color: '#fff',
+  },
   buttonFood: {
-    width: Dimensions.get('screen').width / 4,
-    backgroundColor: '#fa6',
+    width: Dimensions.get('screen').width / 2,
+    height: 45,
+    backgroundColor: '#ea565c',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 4,
   },
   buttonDrink: {
-    width: Dimensions.get('screen').width / 4,
-    backgroundColor: '#a5f',
+    width: Dimensions.get('screen').width / 2,
+    height: 45,
+    backgroundColor: '#6b63ff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  textButtonsMenu: {
+    fontSize: 22,
+    color: '#fff',
+  },
+  containerFood: {
+    flex: 1,
+  },
+  titleFood: {
+    fontSize: 20,
+    padding: 20,
+    textAlign: 'center',
   },
   input: {
     width: Dimensions.get('screen').width,
@@ -164,5 +182,9 @@ const Styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 18,
+  },
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'center',
   }
 });
